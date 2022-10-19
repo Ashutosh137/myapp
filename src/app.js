997b8bebf232c4f4709bd637bf15162d53a705be
Naresh Thapa <thapanaresh8412@gmail.com>
2022-10-19 13:13:59 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express=require("express");
 const app =express();
+const path=require("path")
+
+require("./db/conn");
 const port =process.env.PORT||3000;
+const static_path=path.join(__dirname,"../public");
+app.use(express.static(static_path))
 app.get("/",(req,res)=>{
 
 
-
+ 
 res.send("hLo World");
 console.log("Request Recived ..........");
 
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,2 +1,16 @@
-const mongoose =requie("mongoose");
-mongoose.connect()
+const mongoose =require("mongoose");
+mongoose.connect("mongodb://localhost:27017/students",{
+
+useNewUrlParser:true,
+ useUnifiedTopology:true,
+ useCreateIndex:true
+}).then(()=>{
+
+
+    console.log('connection sucessful');
+}).catch((e)=>{
+
+
+console.log('no connection');
+
+})
